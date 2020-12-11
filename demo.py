876a7772999d14bf1d9088b82035975b6af99f13
mhkane <hassanmohamed0407@gmail.com>
2020-12-11 04:52:54 -0500

    Simpler features

--- demo.py
+++ demo.py
@@ -7,13 +7,7 @@ nubia = Nubia()
 def predict(inp_1, inp_2):
     features = nubia.score(inp_1, inp_2, get_features=True)
     labels = {k: v for k, v in features["features"].items()}
-    feature_dict = {}
-    feature_dict["Semantic Relationship (out 5.0)"]=features["semantic_relation"]
-    feature_dict["Percentage logical agreement"]=features["logical_agreement"]
-    feature_dict["Percentage irrelevancy or new information"]= features["irrelevancy"]
-    feature_dict["Percentage contradiction"]= features["contradiction"]
-    labels = dict(zip(friendly_keys, list(labels.values()))) 
-    return {"nubia_score": features["nubia_score"]}, feature_dict
+    return {"nubia_score": features["nubia_score"]}, labels
 
 
 title = "NUBIA"
