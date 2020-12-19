3a2859eb41a3bb4a1fa18485e76cf5766a647380
mhkane <hassanmohamed0407@gmail.com>
2020-12-19 00:35:16 -0500

    A bit more complicated examples

--- demo.py
+++ demo.py
@@ -21,13 +21,13 @@ def predict(inp_1, inp_2):
 
 
 title = "NUBIA: A Neural Metric for Text Generation"
-description = "NUBIA stands for 'NeUral Based Interchangeability Assessor'. \n NUBIA gives a score on a scale of 0 to 1 reflecting how much it thinks the candidate text is interchangeable with the reference text. It also shows its rationale for the score."
+description = "NUBIA stands for 'NeUral Based Interchangeability Assessor'. \n NUBIA gives a score on a scale of 0 to 1 reflecting how much it thinks the candidate text is interchangeable with the reference text. It also shows its rationale for the score obtained by comparing the candidate and reference text."
 inputs = [gradio.inputs.Textbox(lines=5, label="Reference Text"), gradio.inputs.Textbox(lines=5, label="Candidate Text")]
 outputs = [gradio.outputs.Label(label="Interchangeability Score"), gradio.outputs.JSON(label="Neural Features (Explanation)")]
 examples = [
     ["This car is expensive! I can't buy it.", "That automobile costs a fortune! Purchasing it? Impossible!"],
     ["This car is expensive! I can't buy it.", "That automobile costs a good amount. Purchasing it? Totally feasible!"],
-    ["The dinner was delicious.", "The dinner did not taste good."]
+    ["The tiger is the second quickest beast. Only the cat is faster than it.", "The second fastest animal in the world is the tiger. The kitten is the only animal faster than the tiger. "]
 ]
 iface = gradio.Interface(fn=predict, inputs=inputs, outputs=outputs, capture_session=True, examples=examples,
                          title=title, description=description, allow_flagging=False)
